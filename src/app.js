c96b07ee8b6df0912e4a9dfa4bbf851ad05a69fa
MireiaGonzalez <mireiagonzalezz03@gmail.com>
2022-01-10 17:11:42 +0000

    exersice in progress

--- src/app.js
+++ src/app.js
@@ -2,13 +2,24 @@
 import "bootstrap";
 import "./style.css";
 
-let allToDos = [];
+var allToDos = [];
 
 const list = document.querySelector("#list");
 let newToDo = document.querySelector("#todo-input");
 
+const deleteToDo = index => {
+  console.log(index);
+  allToDos.splice(index, 1);
+  console.log(allToDos);
+};
+
 const addToDo = () => {
   let str = "";
+  allToDos.forEach((todo, index) => {
+    str += `<li><strong>${todo}</strong> <button onclick="deleteToDo(${index})" type="button" class="delete"> x </button></li>`;
+  });
+  list.innerHTML = str;
+  console.log(list);
 };
 
 newToDo.addEventListener("keypress", e => {
@@ -17,5 +28,6 @@ newToDo.addEventListener("keypress", e => {
     allToDos.push(e.target.value);
     console.log(allToDos);
     addToDo();
+    e.target.value = "";
   }
 });
